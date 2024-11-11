9e5d94e7fd2b456deb255e9ca2094564a526c3c0
itsJoel59 <joel.j.jansson@gmail.com>
2024-11-11 17:54:46 +0100

    Added drop down menu with appropriate styling with JS and CSS

---
+++ main.js
@@ -0,0 +1,54 @@
+const header = document.querySelector('#header');
+const menuIconContainer = document.createElement('div');
+menuIconContainer.setAttribute('id', 'mobile-menu-icon');
+
+const menuIcon = document.createElement('span');
+menuIcon.setAttribute('id', 'menu-icon');
+menuIcon.innerHTML = "<img src='resources/Group 7.png'>";
+menuIconContainer.appendChild(menuIcon);
+header.appendChild(menuIconContainer);
+
+const overlay = document.createElement('div');
+overlay.setAttribute('class', 'overlay');
+// menu.setAttribute('class', 'hidden');
+document.querySelector('body').appendChild(overlay);
+const menu = document.createElement('div');
+menu.setAttribute('id', 'mobile-menu');
+overlay.appendChild(menu);
+const closeBtnWrapper = document.createElement('div');
+closeBtnWrapper.setAttribute('id', 'close-button-wrapper');
+const closeBtn = document.createElement('span');
+closeBtn.setAttribute('id', 'menu-close-button');
+closeBtn.innerHTML = "<img src='resources/close.png'>";
+closeBtnWrapper.appendChild(closeBtn);
+menu.appendChild(closeBtnWrapper);
+
+const menuLinks = document.createElement('div');
+menuLinks.setAttribute('id', 'menu-links');
+menu.appendChild(menuLinks);
+const links = [
+    'Play online', 
+    'Play on-site', 
+    'The story', 
+    'Contact us'
+];
+
+links.forEach((element) => {
+
+    const link = document.createElement('a');
+    link.innerText = element;
+    link.setAttribute('class', 'link roboto-bold');
+    menuLinks.appendChild(link);
+
+})
+
+menuIcon.addEventListener(
+
+    "click", 
+    function(event) {
+
+        
+
+    }
+)
+
